094839892196066891e1488f42086a971cb3b0a2
miozune <miozune@gmail.com>
2022-10-08 04:40:44 +0900

    Unbind toggle focus key by default (#189)

--- src/main/java/appeng/client/ActionKey.java
+++ src/main/java/appeng/client/ActionKey.java
@@ -3,7 +3,7 @@ package appeng.client;
 import org.lwjgl.input.Keyboard;
 
 public enum ActionKey {
-    TOGGLE_FOCUS(Keyboard.KEY_TAB);
+    TOGGLE_FOCUS(Keyboard.KEY_NONE);
 
     private final int defaultKey;
 
