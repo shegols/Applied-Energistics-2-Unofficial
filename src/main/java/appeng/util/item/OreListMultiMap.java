00f978289cfc59c41e80a801842e1c256fa1eaf0
Glease <4586901+Glease@users.noreply.github.com>
2022-12-21 17:18:37 +0800

    fix potential NPE (#212)

--- src/main/java/appeng/util/item/OreListMultiMap.java
+++ src/main/java/appeng/util/item/OreListMultiMap.java
@@ -5,6 +5,8 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableListMultimap.Builder;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
 
 public class OreListMultiMap<T> {
     private ImmutableListMultimap<Integer, T> map;
@@ -17,7 +19,11 @@ public class OreListMultiMap<T> {
         } else {
             s = (AEItemStack) stack;
         }
-        return s.getDefinition().getIsOre().getOres();
+        OreReference ore = s.getDefinition().getIsOre();
+        if (ore == null) return Collections.emptyList();
+        Set<Integer> ids = ore.getOres();
+        if (ids == null) return Collections.emptyList();
+        return ids;
     }
 
     public void put(IAEItemStack key, T val) {
