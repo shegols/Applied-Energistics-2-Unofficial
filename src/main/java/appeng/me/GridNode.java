84574778291f54e4bf32e545fd8d49c2e8d8f624
Pilzinsel64 <pilzinsel64@gmx.de>
2023-03-20 12:31:09 +0100

    Don't prevent GridNode from destroying (#289)
    * prevent adding to new Grid when destroyed
    
    * use isAlive bool instead destroyStatus int

--- src/main/java/appeng/me/GridNode.java
+++ src/main/java/appeng/me/GridNode.java
@@ -52,6 +52,7 @@ public class GridNode implements IGridNode, IPathItem {
     private int compressedData = 0;
     private int usedChannels = 0;
     private int lastUsedChannels = 0;
+    private boolean isAlive = true;
 
     public GridNode(final IGridBlock what) {
         this.gridProxy = what;
@@ -103,7 +104,7 @@ public class GridNode implements IGridNode, IPathItem {
     void validateGrid() {
         final GridSplitDetector gsd = new GridSplitDetector(this.getInternalGrid().getPivot());
         this.beginVisit(gsd);
-        if (!gsd.isPivotFound()) {
+        if (!gsd.isPivotFound() && isAlive) {
             final IGridVisitor gp = new GridPropagator(new Grid(this));
             this.beginVisit(gp);
         }
@@ -212,6 +213,8 @@ public class GridNode implements IGridNode, IPathItem {
 
     @Override
     public void destroy() {
+        isAlive = false;
+
         while (!this.connections.isEmpty()) {
             // not part of this network for real anymore.
             if (this.connections.size() == 1) {
