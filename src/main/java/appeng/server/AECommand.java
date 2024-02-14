86ae541823f14e260bce9f29889c473d81cc2e23
glowredman <35727266+glowredman@users.noreply.github.com>
2024-02-14 21:00:01 +0100

    Stop overriding compareTo in AECommand (#462)

--- src/main/java/appeng/server/AECommand.java
+++ src/main/java/appeng/server/AECommand.java
@@ -34,14 +34,6 @@ public final class AECommand extends CommandBase {
         return 0;
     }
 
-    /**
-     * wtf?
-     */
-    @Override
-    public int compareTo(final Object arg0) {
-        return 1;
-    }
-
     @Override
     public String getCommandName() {
         return "ae2";
